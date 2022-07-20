345b4fdb2ef26e6d3a58b6a5e5279c7d17d18dc6
carlatakagi <carlatakagi@gmail.com>
2022-07-20 18:27:26 -0400

    deploy

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const api = require('./src/data/api.json');
+const data = require('./src/data/api.json');
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/cliente/ativos/:codCliente', (_req, res) => {
-  return res.json(api);
+app.get('/cliente/ativos/:codCliente', (req, res) => {
+  const { CodCliente } = req.params;
+
+  const filteredData = data.filter((equity) => CodCliente === equity.CodCliente)
+  return res.json(filteredData);
 })
 
 app.post('/investimentos/comprar', (_req, res) => {
-  return res.json(api);
+  return res.json(data);
 })
 
 app.post('/investimentos/vender', (_req, res) => {
-  return res.json(api);
+  return res.json(data);
 })
 
 app.listen(port, () => {
