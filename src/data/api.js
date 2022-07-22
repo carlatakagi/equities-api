9aec66d5a423915acbfb86d974082273761ad6be
carlatakagi <carlatakagi@gmail.com>
2022-07-22 16:59:14 -0400

    feat: removing and creating new data file

---
+++ src/data/api.js
@@ -0,0 +1,52 @@
+export const equitiesData = [
+  {
+    CodAtivo: 'XPBR31',
+    QtdeAtivo: 20,
+    Valor: 94.31,
+  },
+  {
+    CodAtivo: 'BBDC4',
+    QtdeAtivo: 10,
+    Valor: 16.44,
+  },
+  {
+    CodAtivo: 'PETR4',
+    QtdeAtivo: 1,
+    Valor: 27.93,
+  },
+  {
+    CodAtivo: 'BBSA3',
+    QtdeAtivo: 4,
+    Valor: 33.28,
+  },
+  {
+    CodAtivo: 'MGLU3',
+    QtdeAtivo: 0,
+    Valor: 2.78,
+  },
+  {
+    CodAtivo: 'VALE3',
+    QtdeAtivo: 1,
+    Valor: 68.37,
+  },
+  {
+    CodAtivo: 'NTCO3',
+    QtdeAtivo: 30,
+    Valor: 15.65,
+  },
+  {
+    CodAtivo: 'MSFT34',
+    QtdeAtivo: 1,
+    Valor: 57.40,
+  },
+  {
+    CodAtivo: 'AAPL34',
+    QtdeAtivo: 1,
+    Valor: 80.58,
+  },
+  {
+    CodAtivo: 'GOGL34',
+    QtdeAtivo: 1,
+    Valor: 79.95,
+  },
+];
