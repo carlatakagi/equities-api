667b2188950b5cedfa51d6eefaa727a20f6f6c1b
carlatakagi <carlatakagi@gmail.com>
2022-07-22 16:58:31 -0400

    feat: add index

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import equitiesRouter from './src/routes/equities.js';
+
 const app = express();
 
-const data = require('./src/data/api.json');
+app.use(express.json());
+app.use(cors());
+app.use('/', equitiesRouter);
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-
-app.get('/cliente/ativos/:codCliente', (req, res) => {
-  const { codCliente } = req.params;
-  const filteredData = data.filter((equity) => Number(codCliente) === equity.CodCliente)
-  return res.json(filteredData);
-})
-
-app.post('/investimentos/comprar', (_req, res) => {
-  return res.json(data);
-})
-
-app.post('/investimentos/vender', (_req, res) => {
-  return res.json(data);
-})
-
 app.listen(port, () => {
-  console.log(`Escutando na porta ${port}`);
+  try {
+    console.log(`Listening port ${port}`);
+  } catch(error) {
+    console.log(`Error ${error}`);
+  }
 });
